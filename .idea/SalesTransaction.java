f0c5c4778fa5bc6831e84abb0b1614b71c79632e
chipa-chipa <129848462+chipa-chipa@users.noreply.github.com>
2024-09-04 16:23:44 +0700

    Getters & Setters
    added getter and setter methods in the SalesTransaction object

--- .idea/SalesTransaction.java
+++ .idea/SalesTransaction.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SalesTransaction {
@@ -17,7 +18,84 @@ public class SalesTransaction {
 
     private double totalAmount;
 
-    // Additional notes about the transaction
     private String additionalNotes;
+
+    public SalesTransaction(int transactionID, LocalDate transactionDate, int clientID, int salesID,
+                            List<String> purchasedItems, double discount, double totalAmount, String notes) {
+        this.transactionID = transactionID;
+        this.transactionDate = transactionDate;
+        this.clientID = clientID;
+        this.salesID = this.salesID;
+        this.purchasedItems = purchasedItems != null ? purchasedItems : new ArrayList<>();
+        this.discount = discount;
+        this.totalAmount = totalAmount;
+        this.additionalNotes = additionalNotes;
+    }
+
+    // Getter and Setter Methods
+    public int getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(int transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public int getClientID() {
+        return clientID;
+    }
+
+    public void setClientID(int clientID) {
+        this.clientID = clientID;
+    }
+
+    public int getSalespersonID() {
+        return salesID;
+    }
+
+    public void setSalespersonID(int salesID) {
+        this.salesID = salesID;
+    }
+
+    public List<String> getPurchasedItems() {
+        return purchasedItems;
+    }
+
+    public void setPurchasedItems(List<String> purchasedItems) {
+        this.purchasedItems = purchasedItems;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getAdditionalNotes() {
+        return additionalNotes;
+    }
+
+    public void setAdditionalNotes(String additionalNotes) {
+        this.additionalNotes = additionalNotes;
+    }
+
 
 }
