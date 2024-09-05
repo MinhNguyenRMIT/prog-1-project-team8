d51cd9e03d005e71cf8ea1846c22e0535b62d8a4
ZanlSl <sonlunct02@gmail.com>
2024-09-05 22:05:06 +0700

    AutoPart CRUD & folders created

---
+++ AutoPart/src/AutoPartInterface.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public interface AutoPartInterface {
+    void addPart(AutoPart part);
+    void removePart(String partId);
+    void updatePart(String partId, AutoPart updatedPart);
+    AutoPart getPart(String partId);
+    List<AutoPart> getAllParts();
+}
+
